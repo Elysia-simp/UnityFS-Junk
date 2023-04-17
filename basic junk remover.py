dd0cf91f9e641e7419467a49964d123b757c8a01
Chips <105132829+Elysia-simp@users.noreply.github.com>
2023-04-16 23:36:39 -0700

    Add files via upload

---
+++ basic junk remover.py
@@ -0,0 +1,30 @@
+import os
+
+directory = input("Enter the directory path: ").replace("\\", "/")
+
+remove_offset = input("y/n automatically skip some data?: ").lower() == 'y'
+
+if remove_offset:
+    seek_start = int(input("input offset? (1, 10, 20, 30): "))
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        file_path = os.path.join(root, file)
+        with open(file_path, 'r+b') as f:
+            if remove_offset:
+                # remove the first 0x14 bytes
+                f.seek(seek_start)
+                data = f.read()
+                f.seek(0)
+                f.write(data)
+                f.truncate(len(data))
+            f.seek(0)
+            try:
+                start = f.read().index(bytes.fromhex('556e6974794653'))
+                f.seek(start)
+                data = f.read()
+                f.seek(0)
+                f.write(data)
+                f.truncate(len(data))
+            except ValueError:
+                pass
